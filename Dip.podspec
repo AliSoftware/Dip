dbc89002708dc4757a7e021d4fe8d5df154f0f97
Ilya Puchka <ilya@puchka.me>
2015-11-04 12:21:52 +0100

    added separate project for Dip framework

--- Dip.podspec
+++ Dip.podspec
@@ -26,5 +26,5 @@ Pod::Spec.new do |s|
   s.platform     = :ios, '8.0'
   s.requires_arc = true
 
-  s.source_files = 'Sources/**/*'
+  s.source_files = 'Dip/Dip/**/*'
 end
---
+++ Dip/Dip/Dip.h
@@ -0,0 +1,19 @@
+//
+//  Dip.h
+//  Dip
+//
+//  Created by Ilya Puchka on 04.11.15.
+//  Copyright © 2015 AliSoftware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Dip.
+FOUNDATION_EXPORT double DipVersionNumber;
+
+//! Project version string for Dip.
+FOUNDATION_EXPORT const unsigned char DipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dip/PublicHeader.h>
+
+
