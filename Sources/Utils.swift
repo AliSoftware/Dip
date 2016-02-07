0515e7358dd726f840d504304bf47133b95234b5
Ilya Puchka <ilya@puchka.me>
2016-02-07 21:05:40 +0100

    fixed getting lock

--- Sources/Utils.swift
+++ Sources/Utils.swift
@@ -63,16 +63,15 @@ extension Optional {
   private func _initializeRecursiveMutex() -> pthread_mutex_t {
     var mutex: pthread_mutex_t = pthread_mutex_t()
     var mta: pthread_mutexattr_t = pthread_mutexattr_t()
-    pthread_mutex_init(&mutex, nil)
     pthread_mutexattr_init(&mta)
-    pthread_mutexattr_settype(&mta, PTHREAD_MUTEX_RECURSIVE)
+    pthread_mutexattr_settype(&mta, Int32(PTHREAD_MUTEX_RECURSIVE))
     pthread_mutex_init(&mutex, &mta)
     return mutex
   }
   
   extension pthread_mutex_t {
     mutating func lock() {
-      pthread_mutex_lock(&self)
+      pthread_mutex_trylock(&self)
     }
     mutating func unlock() {
       pthread_mutex_unlock(&self)
