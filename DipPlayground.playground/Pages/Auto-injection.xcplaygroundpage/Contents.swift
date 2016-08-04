2a0e2e5493fd65004812fa389118e603b215fa8e
Ilya Puchka <ilyapuchka@gmail.com>
2016-08-04 23:54:21 +0200

    fixed auto-injection playground page

--- DipPlayground.playground/Pages/Auto-injection.xcplaygroundpage/Contents.swift
+++ DipPlayground.playground/Pages/Auto-injection.xcplaygroundpage/Contents.swift
@@ -115,7 +115,10 @@ class ServerImp: Server {
     weak var client: ServerClient?
 }
 
-class ServerClientImp: ServerClient {
+//There is currently a bug in Swift that causes runtime crash
+//when trying to auto-inject not-NSObject weak property.
+//https://bugs.swift.org/browse/SR-2144
+class ServerClientImp: NSObject, ServerClient {
     var server: Server?
     
     init(server: Server) {
@@ -148,7 +151,10 @@ class InjectedServerImp: Server {
     var client: ServerClient? { return injectedClient.value }
 }
 
-class InjectedClientImp: ServerClient {
+//There is currently a bug in Swift that causes runtime crash 
+//when trying to auto-inject not-NSObject weak property.
+//https://bugs.swift.org/browse/SR-2144
+class InjectedClientImp: NSObject, ServerClient {
     private var injectedServer = Injected<Server>()
     var server: Server? { get { return injectedServer.value } }
 }
