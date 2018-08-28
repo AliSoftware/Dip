5a8e34b506409f26277ae647c5b1b1e65e2451ac
Steven Kramer <steven@m20.nl>
2018-08-28 12:26:33 +0200

    Fix for Swift 4.2 changes in Optional<->Any conversions

--- Sources/Resolve.swift
+++ Sources/Resolve.swift
@@ -199,7 +199,7 @@ extension DependencyContainer {
      That happens because when Optional is casted to Any Swift can not implicitly unwrap it with as operator.
      As a workaround we detect boxing here and unwrap it so that we return not a box, but wrapped instance.
      */
-    if let box = resolvedInstance as? BoxType, let unboxed = box.unboxed as? T {
+    if let box = resolvedInstance as? BoxType, let unboxedAny = box.unboxed, let unboxed = unboxedAny as? T {
       resolvedInstance = unboxed
     }
     
